72a65da39a1629dca7f25ef47ccf07c209d2e3e9
sh2005am <123765720+sh2005am@users.noreply.github.com>
2025-07-30 01:23:59 +0530

    Update apiConfig.js

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
 const apiUrl = import.meta.env.DEV
   ? '' // For local development
-  : 'https://think-box-project.onrender.com/'; // <-- Your live Render URL
+  : 'https://think-box-project.onrender.com'; // <-- Your live Render URL
 export default apiUrl;
