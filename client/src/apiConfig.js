6d56c3843e7efe983c9ea5b1185ab4afa73d65d5
xenon <shadowonart@gmail.com>
2025-07-30 00:31:43 +0530

    final api cofig for deployment

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
 const apiUrl = import.meta.env.DEV
   ? '/api' // For local development
-  : 'https://think-box-api.onrender.com/api'; // <-- Your live Render URL
+  : 'https://think-box-project.onrender.com/api'; // <-- Your live Render URL
 export default apiUrl;
